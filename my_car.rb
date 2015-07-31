3f39145617edc9c191b0c5eacb6f3b7783cbc685
bryantt23 <bryantt23@gmail.com>
2015-07-31 03:37:27 -0700

    classes 2

--- my_car.rb
+++ my_car.rb
@@ -26,6 +26,17 @@ class MyCar
     self.color=new_color
   end
 
+  def to_s
+    "This car is a #{year} #{color} #{model}"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+  MyCar.gas_mileage(13, 351)  # => "27 miles per gallon of gas"
+
+
+
   honda = MyCar.new(98, "blue", "civic")
   puts honda.year
   puts honda.speed
@@ -35,5 +46,6 @@ class MyCar
   puts honda.shut_down
   honda.spray_paint("black")
   puts honda.color
+  puts honda
 
 end
